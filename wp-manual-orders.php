0a11acf203eb61936186789843498e3469f10794
nahid <nfmohit49@gmail.com>
2018-04-05 11:52:09 +0600

    Quick fix - Typo in plugin description

--- wp-manual-orders.php
+++ wp-manual-orders.php
@@ -6,7 +6,7 @@
 /*
 Plugin Name: WordPress Manual Orders
 Plugin URI: https://nfmohit.pro/plugins/customer-panel
-Description: A simple plugin that lets you add manualfline orders on behalf of your customers so that they can login and see their purchase history in a simple clean interface.
+Description: A simple plugin that lets you add manual orders on behalf of your customers so that they can login and see their purchase history in a simple clean interface.
 Author: Nahid Ferdous Mohit
 Version: 0.0.1
 Author URI: https://nfmohit.pro
